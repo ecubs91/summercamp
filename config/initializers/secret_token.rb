6c1a4c73862108b057f0e36614ec630ffd1ef200
ecubs91 <harrylee1230@gmail.com>
2014-08-08 11:24:55 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Summercamp::Application.config.secret_key_base = 'f6443249507afe2755781ab4d80d77b2dfa4d8312e1368ff9fbc949f9bf5866ada75fe0387b298c555ab7481b1212c065c642c90daef6f9b414e59103b446592'
